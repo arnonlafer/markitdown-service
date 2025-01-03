3daab5f5541eabe5a1fbb9229788cf3f92c8b172
Arnon Lafer <arnon@tonkean.com>
2025-01-03 16:53:52 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, File, UploadFile
-from markitdown import convert_to_markdown
+import shutil
+from markitdown import MarkItDown
+from fastapi import FastAPI, UploadFile
+from uuid import uuid4
+
+md = MarkItDown()
 
 app = FastAPI()
 
 @app.post("/convert")
-async def convert(file: UploadFile = File(...)):
-    try:
-        content = await file.read()
-        markdown = convert_to_markdown(content)
-        return {"markdown": markdown}
-    except Exception as e:
-        return {"error": str(e)}
+async def convert_markdown(file: UploadFile):
+    unique_id = uuid4()
+    temp_dir = f"./temp/{unique_id}"
+
+    shutil.os.makedirs(temp_dir, exist_ok=True)
+
+    file_path = f"{temp_dir}/{file.filename}"
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    result = md.convert(file_path)
+    content = result.text_content
+
+    shutil.rmtree(temp_dir)
+
+    return {"result": content}
